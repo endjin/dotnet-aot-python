985f2a9a7d30698f0c1a8818db7cba2f7972f65b
Howard van Rooijen <Howard.vanRooijen@endjin.com>
2024-01-07 21:38:14 +0000

    Update to run inside a devcontainer for linux-x64

--- build.ps1
+++ build.ps1
@@ -1,6 +1,8 @@
 $dotnetInfo = dotnet --info
 $runtimeIdentifier = $dotnetInfo | Select-String -Pattern "RID:" | ForEach-Object { $_.ToString().Split(":")[1].Trim() }
 
+Write-Output "Building for $runtimeIdentifier"
+
 dotnet publish ./dotnet/daeta.utils/solutions/daeta.utils/daeta.utils.csproj -r $runtimeIdentifier -o ./artefacts/$runtimeIdentifier/
 
 python ./python/dotnet-native-lib.py "$runtimeIdentifier"
--- python/dotnet-native-lib.py
+++ python/dotnet-native-lib.py
@@ -2,9 +2,10 @@ import sys
 from ctypes import *
 
 architecture = sys.argv[1]
+extension = 'dll' if architecture =="win-x64" else 'so'
 
-dotnetnativelib = cdll.LoadLibrary(f"./artefacts/{architecture}/daeta.utils.dll")
+dotnetnativelib = cdll.LoadLibrary(f"./artefacts/{architecture}/daeta.utils.{extension}")
 dotnetnativelib.multiply.argtypes = [c_int, c_int]
 dotnetnativelib.multiply.restype = c_int
 
-print(dotnetnativelib.multiply(20, 24))
+print(f"20 x 24 = {dotnetnativelib.multiply(20, 24)}")
