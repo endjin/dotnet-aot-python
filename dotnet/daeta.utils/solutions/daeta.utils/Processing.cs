ed68f8c57ac005598f3543f32ec91d6d3f3b201c
Howard van Rooijen <Howard.vanRooijen@endjin.com>
2024-01-09 09:05:56 +0000

    Add example which shows how to pass a string.

--- dotnet/daeta.utils/solutions/daeta.utils/Processing.cs
+++ dotnet/daeta.utils/solutions/daeta.utils/Processing.cs
@@ -1,5 +1,5 @@
-﻿using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+﻿using System.Runtime.InteropServices;
+using Spectre.Console;
 
 namespace daeta.utils;
 
@@ -7,4 +7,24 @@ public static class Processing
 {
     [UnmanagedCallersOnly(EntryPoint = "multiply")]
     public static int Multiply(int x, int y) => x * y;
+
+    [UnmanagedCallersOnly(EntryPoint = "write_line")]
+    public static int WriteLine(IntPtr pMessage)
+    {
+        try
+        {
+            string? message = Marshal.PtrToStringAnsi(pMessage);
+
+            if (message is not null)
+            {
+                AnsiConsole.MarkupLine($"[red]{message}[/] via [green]Spectre.Console[/]");
+            }
+        }
+        catch
+        {
+            return -1;
+        }
+
+        return 0;
+    }
 }
--- python/dotnet-native-lib.py
+++ python/dotnet-native-lib.py
@@ -8,4 +8,9 @@ dotnetnativelib = cdll.LoadLibrary(f"./artefacts/{architecture}/daeta.utils.{ext
 dotnetnativelib.multiply.argtypes = [c_int, c_int]
 dotnetnativelib.multiply.restype = c_int
 
+dotnetnativelib.write_line.argtypes = [c_char_p]
+dotnetnativelib.write_line.restype = c_int
+
 print(f"20 x 24 = {dotnetnativelib.multiply(20, 24)}")
+
+dotnetnativelib.write_line(c_char_p(b"Hello from Python!"))
